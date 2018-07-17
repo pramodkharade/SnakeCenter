d36ff38ee83ed5b43dc584df93324c85d2972f0f
pramod.kharade <pramod.kharade@blazeclan.com>
2018-07-17 14:58:32 +0530

    dynamic and clickable menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadFeature = 'recipe';
+  onNavigate(feature:string){
+      this.loadFeature = feature;
+  }
   
 }
