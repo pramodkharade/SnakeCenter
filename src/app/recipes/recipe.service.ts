837063507ce496b9952522828d794083db820cd9
pramod.kharade <pramod.kharade@blazeclan.com>
2018-07-20 16:09:18 +0530

    ingredient with services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,39 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
-
+@Injectable()
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe','This is simply a test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('A Test Recipe 2','This is simply a test 2','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+            'Tasty Schnitzel',
+            'A super-tasty Schnitzel - just awesome',
+            'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+            [
+                new Ingredient('Meat',1),
+                new Ingredient('French Fries',20)
+            ]    
+        ),
+        new Recipe(
+            'Big Fat Burger',
+            'What else you need to say?',
+            'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+        [
+            new Ingredient('Buns',2),
+            new Ingredient('Meat',5)
+        ]
+        ),
+        new Recipe(
+            'Wada Paw',
+            'It is testy Wadapaw made by Shital bari',
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+            new Ingredient('Buns',2),
+            new Ingredient('Potatos',1),
+            new Ingredient('chilly',1)
+        ]
+        )
       ];
       getRecipes(){
           return this.recipes.slice();
