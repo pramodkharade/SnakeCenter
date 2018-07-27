cb90ca0a409826a444e7ed3697fa888b29859545
pramod.kharade <pramod.kharade@blazeclan.com>
2018-07-27 15:37:39 +0530

    savedata and getdata from server using http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from "@angular/http";
 import { RecipeService } from './recipes/recipe.service';
+import { DataStorageService } from './shared/data.storage.service';
 
 
 @NgModule({
@@ -41,7 +42,7 @@ import { RecipeService } from './recipes/recipe.service';
     ReactiveFormsModule,
     HttpModule
   ],
-  providers: [ShoppingListService,RecipeService],
+  providers: [ShoppingListService,RecipeService,DataStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
