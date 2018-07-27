e9c6a63afbce7feccc58ea660b552903d03f2eba
pramod.kharade <pramod.kharade@blazeclan.com>
2018-07-27 18:58:12 +0530

    login authentication for save data and fetch data with logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import * as firebase from 'firebase';
 @Injectable()
 export class AuthService {
+    token: string;
     signupUser(email:string , password: string){
         firebase.auth().createUserWithEmailAndPassword(email,password)
         .catch(
@@ -11,9 +12,26 @@ export class AuthService {
     signinUser(email:string,password:string){
         firebase.auth().signInWithEmailAndPassword(email,password)
         .then(
-            response=>console.log(response)
+            response=>{
+                firebase.auth().currentUser.getIdToken().then(
+                    (token:string)=>this.token = token
+                )
+            }
         ).catch(
             error=> console.log(error)
         );
+    }
+    getToken(){
+         firebase.auth().currentUser.getIdToken().then(
+            (token:string)=>this.token = token
+        );
+        return this.token
+    }
+    isAuthenticated(){
+        return this.token !=null;
+    }
+    logout(){
+        firebase.auth().signOut();
+        this.token = null;
     }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,16 @@
 import { Component } from "@angular/core";
 import { Response } from '@angular/http';
 import { DataStorageService } from "../shared/data.storage.service";
+import { AuthService } from "../auth/auth.service";
 
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html'
 })
 export class HeaderComponent{
-     constructor(private dataStorageService:DataStorageService){}
+     constructor(
+         private dataStorageService:DataStorageService,
+        private authService:AuthService){}
     onSaveData(){
         this.dataStorageService.storeRecipes().subscribe(
             (response:Response)=>{
@@ -18,6 +21,9 @@ export class HeaderComponent{
     }
     onGetData(){
         this.dataStorageService.getRecipes();
+    }
+    onLogout(){
+        this.authService.logout();
     }
     
 }
