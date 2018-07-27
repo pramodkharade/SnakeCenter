dc6c4932b9b0386c2f1e6b5a8c232a997e6feaae
pramod.kharade <pramod.kharade@blazeclan.com>
2018-07-27 17:36:37 +0530

    initialized firbase app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadFeature = 'recipe';
   onNavigate(feature:string){
       this.loadFeature = feature;
+  }
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyChWygc6KYib6Rvp9GROKARZMxh9eHt-R0",
+      authDomain: "snackrecipebook.firebaseapp.com",
+  
+    });
   }
   
 }
