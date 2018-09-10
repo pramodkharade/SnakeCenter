f2d0e594fb930f2fb9fbd234b3513b23c84cfb02
pramod.kharade <pramod.kharade@blazeclan.com>
2018-09-10 10:50:40 +0530

    format the code

--- src/app/shared/data.storage.service.ts
+++ src/app/shared/data.storage.service.ts
@@ -1,34 +1,35 @@
-import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
-import { RecipeService } from "../recipes/recipe.service";
-import { Recipe } from "../recipes/recipe.model";
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+// tslint:disable-next-line:import-blacklist
 import 'rxjs/Rx';
-import { AuthService } from "../auth/auth.service";
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
-export class DataStorageService{
+export class DataStorageService {
     constructor(
-        private http:Http,
-        private recipeService:RecipeService,
-    private authService:AuthService){}
+        private http: Http,
+        private recipeService: RecipeService,
+    private authService: AuthService) {}
 
-    storeRecipes(){
+    storeRecipes() {
         const token = this.authService.getToken();
-       return this.http.put('https://snackrecipebook.firebaseio.com/recipes.json?auth='+ token,this.recipeService.getRecipes())
+       return this.http.put('https://snackrecipebook.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes())
     }
-    getRecipes(){
+    getRecipes() {
         const token = this.authService.getToken();
-        this.http.get('https://snackrecipebook.firebaseio.com/recipes.json?auth='+ token)
-        .map((response:Response)=>{
-            const recipes:Recipe[] = response.json();
-            for(let recipe of recipes){
-                if(!recipe['ingredients']){
-                    recipe['ingredients']=[];
+        this.http.get('https://snackrecipebook.firebaseio.com/recipes.json?auth=' + token)
+        .map((response: Response) => {
+            const recipes: Recipe[] = response.json();
+            for (let recipe of recipes) {
+                if (!recipe['ingredients']) {
+                    recipe['ingredients'] = [];
                 }
             }
             return recipes;
         })
-        .subscribe((recipes:Recipe[])=>{
+        .subscribe((recipes: Recipe[]) => {
             this.recipeService.setRecipes(recipes);
         });
     }
