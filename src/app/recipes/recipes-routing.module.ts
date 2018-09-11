88b907bc558a2932ae7a811e9604d2596f729b53
pramod.kharade <pramod.kharade@blazeclan.com>
2018-09-11 12:19:12 +0530

    optimazted the code by separting the auth,signup and shoppinglist module and routing

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,25 +1,24 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router"
-import { RecipesComponent } from "./recipes.component";
-import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
-import { AuthGuard } from "../auth/auth-guard.service";
-import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { AuthGuard } from '../auth/auth-guard.service';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 const recipesRoutes: Routes = [
-    { path:'recipes',component:RecipesComponent, children:[
-        {path:'',component:RecipeStartComponent},
-        { path:"new",component:RecipeEditComponent,canActivate:[AuthGuard]},
-        { path:":id",component:RecipeDetailComponent},
-        
-        { path:":id/edit",component:RecipeEditComponent,canActivate:[AuthGuard]}
+    { path: 'recipes', component: RecipesComponent, children: [
+        {path: '', component: RecipeStartComponent},
+        { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]},
+        { path: ':id', component: RecipeDetailComponent},
+        { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard]}
     ] }
 ];
 @NgModule({
-    imports:[
+    imports: [
         RouterModule.forChild(recipesRoutes)
     ],
-    exports:[RouterModule]
+    exports: [RouterModule]
 })
-export class RecipesRoutingModule{
+export class RecipesRoutingModule {
 
 }
