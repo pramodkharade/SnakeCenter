cee8acc39a22788715411eca3bd6376087242d9d
pramod.kharade <pramod.kharade@blazeclan.com>
2018-09-11 16:52:17 +0530

    lazyloading and optimazation for services and authguard

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,21 +3,31 @@ import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipes/recipe.service';
+import { DataStorageService } from '../shared/data.storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @NgModule({
-    declarations:[
+    declarations: [
         HeaderComponent,
         HomeComponent
     ],
-    imports:[
+    imports: [
         SharedModule,
         AppRoutingModule
     ],
-    exports:[
+    exports: [
         AppRoutingModule,
         HeaderComponent
+    ],
+    providers: [
+        ShoppingListService,
+        RecipeService,
+        DataStorageService,
+        AuthService,
     ]
 })
-export class CoreModule{
+export class CoreModule {
 
 }
