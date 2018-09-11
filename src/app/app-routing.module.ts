f3c085d53246e173a75824207af5cf25f2c782d8
pramod.kharade <pramod.kharade@blazeclan.com>
2018-09-11 12:37:33 +0530

    added Home component to landing page#1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
