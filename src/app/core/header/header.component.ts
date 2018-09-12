1deb0fdd27893089f31076b130e53ac05b50363e
pramod.kharade <pramod.kharade@blazeclan.com>
2018-09-12 10:20:09 +0530

    configuration request and response for httpClient

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Response } from '@angular/http';
+import { HttpEvent } from '@angular/common/http';
 import { DataStorageService } from '../../shared/data.storage.service';
 import { AuthService } from '../../auth/auth.service';
 
@@ -13,7 +13,7 @@ export class HeaderComponent {
         public authService: AuthService) {}
     onSaveData() {
         this.dataStorageService.storeRecipes().subscribe(
-            (response: Response) => {
+            (response: HttpEvent<object>) => {
                 console.log(response);
             }
         );
--- src/app/shared/data.storage.service.ts
+++ src/app/shared/data.storage.service.ts
@@ -16,7 +16,10 @@ export class DataStorageService {
 
     storeRecipes() {
         const token = this.authService.getToken();
-       return this.httpClient.put('https://snackrecipebook.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes())
+       return this.httpClient.put('https://snackrecipebook.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes(),
+       {
+           observe: 'body'
+       })
     }
     getRecipes() {
         const token = this.authService.getToken();
