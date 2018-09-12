f11363b9e972b3decf16ff501c0e72db15857373
pramod.kharade <pramod.kharade@blazeclan.com>
2018-09-12 10:33:17 +0530

    query string by using HttpClient

--- src/app/shared/data.storage.service.ts
+++ src/app/shared/data.storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 // tslint:disable-next-line:import-blacklist
@@ -16,16 +16,18 @@ export class DataStorageService {
 
     storeRecipes() {
         const token = this.authService.getToken();
-       return this.httpClient.put('https://snackrecipebook.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes(),
+       return this.httpClient.put('https://snackrecipebook.firebaseio.com/recipes.json', this.recipeService.getRecipes(),
        {
-           observe: 'body'
+           observe: 'body',
+           params: new HttpParams().set('auth', token)
        })
     }
     getRecipes() {
         const token = this.authService.getToken();
-        this.httpClient.get<Recipe[]>('https://snackrecipebook.firebaseio.com/recipes.json?auth=' + token
+        this.httpClient.get<Recipe[]>('https://snackrecipebook.firebaseio.com/recipes.json'
         { 
             observe: 'body',
+            params: new HttpParams().set('auth', token),
             responseType: 'json'
         })
         .map((recipes) => {
