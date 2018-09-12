59ae1c3621f337c1e6b889f8c26039c3f00fd3e6
pramod.kharade <pramod.kharade@blazeclan.com>
2018-09-12 11:58:09 +0530

    multiple Interceptors along with logging interceptor

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,11 +1,11 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
-import { Observable } from 'rxjs/observable';
 import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
 import { AuthService } from '../auth/auth.service';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
     constructor(private authService: AuthService){}
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('Interceptor........',req);
         //const copiedReq = req.clone({headers:req.headers.set('','')});
         const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
